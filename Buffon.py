3f7d531a33536cef945166886aa133023ee9ecc1
Andrew J. Haeffner <andrewhaeffner@outlook.com>
2018-05-27 01:09:47 -0400

    Added long needle simulation. Exercise 8 is complete. Done with needles for now.

--- Buffon.py
+++ Buffon.py
@@ -29,6 +29,16 @@ def laplace_trial(condition, n):
     return counter / float(n)
 
 
+def long_needle_trial(n, L):
+
+    crosses = 0.0
+    for i in range(n):
+        theta =  random() * pi * .5
+        crosses += L * (cos(theta) + sin(theta))
+
+    return crosses / n
+
+
 if __name__ == '__main__':
     e = easy_buffon
     r = run_trial
@@ -54,3 +64,15 @@ if __name__ == '__main__':
         print('Pi for ', s[i], ' trials:')
         print('\t', 3.0/L[i])
     print('----------------------------------')
+
+
+    r = long_needle_trial
+    l = 100
+    L = [r(s[0],l), r(s[1],l), r(s[2],l)]
+    print('Running Trials for Long Needlers.')
+    print('----------------------------------')
+    for i in range(len(L)):
+        print('Pi for ', s[i], ' trials:')
+        print('\t', 4.0*l/L[i])
+    print('----------------------------------')
+    
