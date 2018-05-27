7b43e553b68c31502b1ceb919b7243bcefea883a
Andrew J. Haeffner <andrewhaeffner@outlook.com>
2018-05-26 23:24:28 -0400

    MonteCarlo Problem Description(s)

---
+++ MonteCarlo.py
@@ -0,0 +1,31 @@
+from random import random
+
+
+# Exercise 3
+#
+# Estimate the area of a circle of radius 1/2
+# with center at (1/2,1/2) inside the unit
+# square by choosing 1000 points at random.
+# Compare your results with the true value of
+# pi/4. Use your results to estimate pi.
+
+
+# Exercise 4
+#
+# Estimate area under the graph of y = sin(pi x)
+# inside the unit square by choosing 10K points
+# at random. Estimate pi.
+
+# Exercise 5
+#
+# Estimate the area under the graph of
+# y = 1/(x+1) in the unit square in the same
+# way as E4. Calculate the true value of this
+# area and use the simulation to estimate the
+# value of log 2.
+
+def __main__():
+    # place holder.
+
+if __name__ == '__main__':
+    main()
