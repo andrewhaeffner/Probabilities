a0165c18d93fc1234df987540a3bea2cb0fc1329
Andrew J. Haeffner <andrewhaeffner@outlook.com>
2018-06-02 20:19:40 -0400

    Cleaning.

--- exponential.py
+++ exponential.py
@@ -7,27 +7,18 @@ def gen_outcome(lam):
     Generates an outcome for an exponential
     distribution, given a lambda.
     '''
-    return -30*np.log(random())
+    return -lam*np.log(random())
 
 def run_trial(start, finish, lam, subintervals, num_of_points):
 
-    results = dict()
-    for i in range(subintervals):
-        results[i] = 0
+    results = [0 for i in range(subintervals)]
 
-    divisions = []
-    current = start
     step = (finish - start) / subintervals
-    for i in range(subintervals + 1):
-        divisions.append(current)
-        current += step
-
+    divisions = [i * step for i in range(subintervals + 1)]
 
     i = 0
     while i < num_of_points:
         outcome = gen_outcome(lam)
-        #print('outcome: ', outcome)
-        #a = input()
         if start < outcome < finish:
             prev = 0
             j = 1
@@ -37,14 +28,10 @@ def run_trial(start, finish, lam, subintervals, num_of_points):
                     break
                 prev = j
                 j += 1
-        else:
-            continue
-        i += 1
+            i += 1
 
-    for event in results:
-        results[event] /= num_of_points
-        results[event] /= ((finish - start) / subintervals)
-        print('yo: ', results[event])
+    for event in range(len(results)):
+        results[event] /= (num_of_points * ((finish - start) / subintervals))
 
     return results
 
@@ -56,7 +43,7 @@ if __name__ == '__main__':
     finish = 120
     lam = 30 # lam signifies the average time between events for an exponential distribution of probabilities.
     subintervals = 24
-    num_of_points = 10000
+    num_of_points = 100000
 
     x = np.linspace(start, finish, 256)
     k = 1/lam
@@ -67,22 +54,9 @@ if __name__ == '__main__':
 
     results = run_trial(start, finish, lam, subintervals, num_of_points)
 
-    divisions = []
-    current = start
     step = (finish - start) / subintervals
-    for i in range(subintervals):
-        divisions.append(current)
-        current += step
+    divisions = [(i * step) for i in range(subintervals)]
 
-    formatted_result = []
-    for result in results:
-        formatted_result.append(results[result])
-
-
-    ax.hist(divisions, subintervals, weights=formatted_result)
+    ax.hist(divisions, subintervals, weights=results)
 
     plt.show()
-
-    for result in results:
-        print('Result #', result)
-        print('\t', results[result])
